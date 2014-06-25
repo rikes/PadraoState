e95a9c6b2159b5cdbf76cbd5af73adbdca2e3021
20121bsi0473 <20121bsi0473@SR6182.cefetes.br>
2014-06-25 11:16:13 -0300

    bla bla

---
+++ src/main/java/br/edu/ifes/edu/model/InterfaceState.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.edu.model;
+
+/**
+ *
+ * @author 20121bsi0473
+ */
+public interface InterfaceState {
+    
+    public void insereMoeda();
+    
+    public void ejetaMoeda();
+    
+    public void acionaMoeda();
+}
---
+++ src/main/java/br/edu/ifes/edu/model/MaquinaDeMascar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.edu.model;
+
+/**
+ *
+ * @author 20121bsi0473
+ */
+public class MaquinaDeMascar implements InterfaceState{
+
+    public void insereMoeda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void ejetaMoeda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void acionaMoeda() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
